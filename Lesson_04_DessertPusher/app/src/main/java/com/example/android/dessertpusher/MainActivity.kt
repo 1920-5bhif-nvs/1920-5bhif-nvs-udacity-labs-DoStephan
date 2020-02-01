c91ebdec9759b813550001adf6ccaee3d47bbb47
DoStephan <stephan.do08@gmail.com>
2020-02-01 09:57:15 +0100

    Do - Add logging message

--- Lesson_04_DessertPusher/app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
+++ Lesson_04_DessertPusher/app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
@@ -18,6 +18,7 @@ package com.example.android.dessertpusher
 
 import android.content.ActivityNotFoundException
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
@@ -64,6 +65,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i("MainActivity", "onCreate called")
 
         // Use Data Binding to get reference to the views
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
@@ -102,7 +104,8 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
     private fun showCurrentDessert() {
         var newDessert = allDesserts[0]
         for (dessert in allDesserts) {
-            if (dessertsSold >= dessert.startProductionAmount) {
+            if (dessertsSold >= dessert.startProducLog.i("MainActivity", "onCreate called")
+                    tionAmount) {
                 newDessert = dessert
             }
             // The list of desserts is sorted by startProductionAmount. As you sell more desserts,
@@ -145,5 +148,9 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
             R.id.shareMenuButton -> onShare()
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onStart() {
+        super.onStart()
     }
 }
